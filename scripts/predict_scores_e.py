fa4fade9c07e1e5e0d003a4dcebe3fdd409a59b5
enriquemondragon <emondra99@gmail.com>
2024-04-10 19:55:31 -0400

    vairant score prediction

---
+++ scripts/predict_scores_e.py
@@ -0,0 +1,256 @@
+
+#!/usr/bin/env python3
+
+'''
+Predict variant scores using Enformer (https://github.com/google-deepmind/deepmind-research/tree/master/enformer)
+
+Author: Mondragon-Estrada E. et al.
+Last reviewed: April 2024
+Copyright (c) Mondragon-Estrada E. et al., 2024
+
+Usage:
+python3 predict_scores_e.py -v ${variants.vcf} -f ${genome.fa} -t ${targets.txt} -o ${outdir}
+'''
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import gzip
+import kipoiseq
+from kipoiseq import Interval
+import pyfaidx
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+import os
+import argparse
+import sys
+import joblib
+import re
+import h5py
+
+#===========================================#
+#      Enformer: Classes and functions      #
+#===========================================#
+
+SEQUENCE_LENGTH = 393216
+
+class Enformer:
+
+  def __init__(self, tfhub_url):
+    self._model = hub.load(tfhub_url).model
+
+  def predict_on_batch(self, inputs):
+    predictions = self._model.predict_on_batch(inputs)
+    return {k: v.numpy() for k, v in predictions.items()}
+
+  @tf.function
+  def contribution_input_grad(self, input_sequence,
+                              target_mask, output_head='human'):
+    input_sequence = input_sequence[tf.newaxis]
+
+    target_mask_mass = tf.reduce_sum(target_mask)
+    with tf.GradientTape() as tape:
+      tape.watch(input_sequence)
+      prediction = tf.reduce_sum(
+          target_mask[tf.newaxis] *
+          self._model.predict_on_batch(input_sequence)[output_head]) / target_mask_mass
+
+    input_grad = tape.gradient(prediction, input_sequence) * input_sequence
+    input_grad = tf.squeeze(input_grad, axis=0)
+    return tf.reduce_sum(input_grad, axis=-1)
+
+
+class EnformerScoreVariantsRaw:
+
+  def __init__(self, tfhub_url, organism='human'):
+    self._model = Enformer(tfhub_url)
+    self._organism = organism
+  
+  def predict_on_batch(self, inputs):
+    ref_prediction = self._model.predict_on_batch(inputs['ref'])[self._organism]
+    alt_prediction = self._model.predict_on_batch(inputs['alt'])[self._organism]
+
+    return alt_prediction.mean(axis=1) - ref_prediction.mean(axis=1)
+
+
+class EnformerScoreVariantsNormalized:
+
+  def __init__(self, tfhub_url, transform_pkl_path,
+               organism='human'):
+    assert organism == 'human', 'Transforms only compatible with organism=human'
+    self._model = EnformerScoreVariantsRaw(tfhub_url, organism)
+    with tf.io.gfile.GFile(transform_pkl_path, 'rb') as f:
+      transform_pipeline = joblib.load(f)
+    self._transform = transform_pipeline.steps[0][1]  # StandardScaler.
+    
+  def predict_on_batch(self, inputs):
+    scores = self._model.predict_on_batch(inputs)
+    return self._transform.transform(scores)
+
+
+class EnformerScoreVariantsPCANormalized:
+
+  def __init__(self, tfhub_url, transform_pkl_path,
+               organism='human', num_top_features=500):
+    self._model = EnformerScoreVariantsRaw(tfhub_url, organism)
+    with tf.io.gfile.GFile(transform_pkl_path, 'rb') as f:
+      self._transform = joblib.load(f)
+    self._num_top_features = num_top_features
+    
+  def predict_on_batch(self, inputs):
+    scores = self._model.predict_on_batch(inputs)
+    return self._transform.transform(scores)[:, :self._num_top_features]
+
+
+#===========================================#
+# Fasta and variants: Classes and functions #
+#===========================================#
+
+class FastaStringExtractor:
+    
+    def __init__(self, fasta_file):
+        self.fasta = pyfaidx.Fasta(fasta_file)
+        self._chromosome_sizes = {k: len(v) for k, v in self.fasta.items()}
+
+    def extract(self, interval: Interval, **kwargs) -> str:
+        # Truncate interval if it extends beyond the chromosome lengths.
+        chromosome_length = self._chromosome_sizes[interval.chrom]
+        trimmed_interval = Interval(interval.chrom,
+                                    max(interval.start, 0),
+                                    min(interval.end, chromosome_length),
+                                    )
+        # pyfaidx wants a 1-based interval
+        sequence = str(self.fasta.get_seq(trimmed_interval.chrom,
+                                          trimmed_interval.start + 1,
+                                          trimmed_interval.stop).seq).upper()
+        # Fill truncated values with N's.
+        pad_upstream = 'N' * max(-interval.start, 0)
+        pad_downstream = 'N' * max(interval.end - chromosome_length, 0)
+        return pad_upstream + sequence + pad_downstream
+
+    def close(self):
+        return self.fasta.close()
+
+
+def variant_generator(vcf_file, gzipped=False):
+  """Yields a kipoiseq.dataclasses.Variant for each row in VCF file."""
+  def _open(file):
+    return gzip.open(vcf_file, 'rt') if gzipped else open(vcf_file)
+    
+  with _open(vcf_file) as f:
+    for line in f:
+      if line.startswith('#'):
+        continue
+      chrom, pos, id, ref, alt_list = line.split('\t')[:5]
+      # Split ALT alleles and return individual variants as output.
+      for alt in alt_list.split(','):
+        yield kipoiseq.dataclasses.Variant(chrom=chrom, pos=pos,
+                                           ref=ref, alt=alt, id=id)
+
+
+def one_hot_encode(sequence):
+  return kipoiseq.transforms.functional.one_hot_dna(sequence).astype(np.float32)
+
+
+def variant_centered_sequences(vcf_file, sequence_length, gzipped=False,
+                               chr_prefix=''):
+  seq_extractor = kipoiseq.extractors.VariantSeqExtractor(
+    reference_sequence=FastaStringExtractor(fasta_file))
+
+  for variant in variant_generator(vcf_file, gzipped=gzipped):
+    print('\nVARIANT: ', variant)
+    interval = Interval(chr_prefix + variant.chrom,
+                        variant.pos, variant.pos)
+    print('INTERVAL: ', interval)
+    interval = interval.resize(sequence_length)
+    center = interval.center() - interval.start
+    print('INTERVAL: ', interval)
+
+    reference = seq_extractor.extract(interval, [], anchor=center)
+    alternate = seq_extractor.extract(interval, [variant], anchor=center)
+
+    print('REFERENCE (first 100bp): ', reference[0:100])
+    print('ALTERNATE (first 100bp): ', alternate[0:100])
+    print('REFERENCE (mid 100bp): ', reference[SEQUENCE_LENGTH//2-50:SEQUENCE_LENGTH//2+50])
+    print('ALTERNATE (mid 100bp): ', alternate[SEQUENCE_LENGTH//2-50:SEQUENCE_LENGTH//2+50])
+    print('REFERENCE (last 100bp): ', reference[SEQUENCE_LENGTH-100:SEQUENCE_LENGTH])
+    print('ALTERNATE (last 100bp): ', alternate[SEQUENCE_LENGTH-100:SEQUENCE_LENGTH])
+    print('num_spaces:',alternate.find('\n'))
+    print('ONE HOT REF: ', one_hot_encode(reference))
+    print('ONE HOT ALT: ', one_hot_encode(alternate))
+    print('ONE HOT REF SHAPE: ', one_hot_encode(reference).shape)
+    print('ONE HOT ALT SHAPE: ', one_hot_encode(alternate).shape)
+    print('VARIANT REF AND ALT: ', variant.ref, variant.alt)
+    
+    yield {'inputs': {'ref': one_hot_encode(reference),
+                      'alt': one_hot_encode(alternate)},
+           'metadata': {'chrom': chr_prefix + variant.chrom,
+                        'pos': variant.pos,
+                        'id': variant.id,
+                        'ref': variant.ref,
+                        'alt': variant.alt}}
+
+
+def main():
+    parser = argparse.ArgumentParser(description=' #============ Predict variant scores with Enformer ============#', usage='%(prog)s')
+    parser.add_argument('-v', '--vcf_file', type=str, required=True, help='file with the genomic variants', dest='vcf_file')
+    parser.add_argument('-f', '--fasta_file', type=str, required=True, help='hg sequence file', dest='fasta_file')
+    parser.add_argument('-t', '--targets_file', type=str, required=True, help='targets file', dest='targets_file')
+    parser.add_argument('-o', '--out_dir', type=str, required=True, help='Output directory', dest='out_dir')
+    
+    args = parser.parse_args()
+    
+    #os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    print(tf.config.list_physical_devices('GPU'))
+    
+    #===========================================#
+    #             Loading data and models       #
+    #===========================================#
+    
+    transform_path = 'gs://dm-enformer/models/enformer.finetuned.SAD.robustscaler-PCA500-robustscaler.transform.pkl'
+    model_path = 'https://tfhub.dev/deepmind/enformer/1'
+    fasta_file = args.fasta_file
+    var_vcf = args.vcf_file
+    # Cite: Kelley et al Cross-species regulatory sequence activity prediction. PLoS Comput. Biol. 16, e1008050 (2020).
+    targets_txt = args.targets_file 
+    df_targets = pd.read_csv(targets_txt, sep='\t')
+    
+    pyfaidx.Faidx(fasta_file)
+    #===========================================#
+    #       Score variants for 5313 targets     #
+    #===========================================#
+    enformer_score_variants_all = EnformerScoreVariantsNormalized(model_path, transform_path)
+
+    # All Scores
+    it = variant_centered_sequences(var_vcf, sequence_length=SEQUENCE_LENGTH,
+                                    gzipped=True, chr_prefix='chr')
+    example_list=[]
+    count = 0
+    for i, example in enumerate(it):
+        print('Done: ', count)
+        count = count + 1
+        variant_scores = enformer_score_variants_all.predict_on_batch(
+            {k: v[tf.newaxis] for k,v in example['inputs'].items()})[0]
+        print('VARIANT SCORE: ', variant_scores, 'SHAPE:',variant_scores.shape)
+        variant_scores = {f'{i}_{name[:20]}': score for i, (name, score) in enumerate(zip(df_targets.description, variant_scores))}
+        example_list.append({**example['metadata'],
+                           **variant_scores})
+
+    df = pd.DataFrame(example_list)
+    df
+    print('\nDATAFRAME IS: \n', df)
+
+    #===========================================#
+    #              Write output file            #
+    #===========================================#
+
+    out_path = args.out_dir + 'sad_scores.csv'
+    df.to_csv (out_path, index = False, header=True, sep ='\t')
+
+    print('saved!')
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/predict_socres_b.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## Predict variant scores using Basenji (https://github.com/calico/basenji)
+
+## Author: Mondragon-Estrada E. et al.
+## Last reviewed: April 2024
+## Copyright (c) Mondragon-Estrada E. et al., 2024
+
+help () {
+         echo -e "\nPredict scores with Basenji"
+         echo -e "Usage: predict_scores_b.sh [outdir] [.vcf]"
+}
+
+if [ $# -ne 6 ]
+then
+    help
+    exit 1
+else
+    outdir=$1
+    vcf=$2
+fi
+
+genome='data/hg38.fa'
+targets='models/human_basenji/targets_human.txt'
+architecture='models/params_human.json'
+model='models/human_basenji/model_human_basenji.h5'
+
+conda activate basenji
+
+basenji_sad.py -f ${genome} \
+-o ${outdir} \
+--rc \
+--shift "1,0,-1" \
+-t ${targets} ${architecture} ${model} ${vcf}
